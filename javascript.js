859732d321e267af9aff3331078a13effa673e00
KohYungChunIvan <s10262491@connect.np.edu.sg>
2024-01-20 15:02:44 +0800

    added spin the wheel

--- javascript.js
+++ javascript.js
@@ -168,3 +168,135 @@ function updateCartItemCount() {
   const cartItems = document.querySelectorAll('.cart-item');
   cartItemCount.textContent = cartItems.length;
 }
+
+
+let wheel = document.querySelector('.wheel');
+let spinBtn = document.querySelector('.spinBtn');
+let value = Math.ceil(Math.random() * 3600);
+
+spinBtn.onclick = function(){
+  wheel.style.transition = "transform 4s ease-out"; // Add a smooth transition
+  wheel.style.transform = "rotate(" + value + "deg)" ;
+  value += Math.ceil(Math.random() * 3600)
+}
+// FOR API
+document.addEventListener("DOMContentLoaded", function () {
+  const APIKEY = "65ab507be8b7cb2cc9ce52f9";
+  getContacts();
+  document.getElementById("update-contact-container").style.display = "none";
+  document.getElementById("add-update-msg").style.display = "none";
+
+  //[STEP 1]: Create our submit form listener
+  document.getElementById("contact-submit").addEventListener("click", function (e) {
+    // Prevent default action of the button 
+    e.preventDefault();
+
+    let userName = document.getElementById("user-name").value;
+    let userEmail = document.getElementById("user-email").value;
+    let userPwd = document.getElementById("user-pwd").value;
+    
+
+    //[STEP 3]: Get form values when the user clicks on send
+    // Adapted from restdb API
+    let jsondata = {
+      "user-name": userName,
+      "user-email": userEmail,
+      "user-pwd": userPwd,
+    };
+
+    //[STEP 4]: Create our AJAX settings. Take note of API key
+    let settings = {
+      method: "POST", //[cher] we will use post to send info
+      headers: {
+        "Content-Type": "application/json",
+        "x-apikey": APIKEY,
+        "Cache-Control": "no-cache"
+      },
+      body: JSON.stringify(jsondata),
+      beforeSend: function () {
+        //@TODO use loading bar instead
+        // Disable our button or show loading bar
+        document.getElementById("contact-submit").disabled = true;
+        
+      }
+    }
+
+    //[STEP 5]: Send our AJAX request over to the DB and print response of the RESTDB storage to console.
+    fetch("https://fedassignment2-7f7e.restdb.io/rest/mori-user", settings)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        document.getElementById("contact-submit").disabled = false;
+        //@TODO update frontend UI 
+        document.getElementById("add-update-msg").style.display = "block";
+        setTimeout(function () {
+          document.getElementById("add-update-msg").style.display = "none";
+        }, 3000);
+        // Update our table 
+        getContacts();
+        // Clear our form using the form ID and triggering its reset feature
+        document.getElementById("add-contact-form").reset();
+      });
+  });
+
+  //[STEP] 6
+  // Let's create a function to allow you to retrieve all the information in your contacts
+  // By default, we only retrieve 10 results
+  function getContacts(limit = 10, all = true) {
+
+    //[STEP 7]: Create our AJAX settings
+    let settings = {
+      method: "GET", //[cher] we will use GET to retrieve info
+      headers: {
+        "Content-Type": "application/json",
+        "x-apikey": APIKEY,
+        "Cache-Control": "no-cache"
+      },
+    }
+
+    //[STEP 8]: Make our AJAX calls
+    // Once we get the response, we modify our table content by creating the content internally. We run a loop to continuously add on data
+    // RESTDb/NoSql always adds in a unique id for each data; we tap on it to have our data and place it into our links 
+    fetch("https://fedassignment2-7f7e.restdb.io/rest/mori-user", settings)
+      .then(response => response.json())
+      .then(response => {
+        let content = "";
+
+        for (var i = 0; i < response.length && i < limit; i++) {
+          //console.log(response[i]);
+          //[METHOD 1]
+          // Let's run our loop and slowly append content
+          // We can use the normal string append += method
+          /*
+          content += "<tr><td>" + response[i].name + "</td>" +
+            "<td>" + response[i].email + "</td>" +
+            "<td>" + response[i].message + "</td>
+            "<td>Del</td><td>Update</td</tr>";
+          */
+
+          //[METHOD 2]
+          // Using our template literal method using backticks
+          // Take note that we can't use += for template literal strings
+          // We use ${content} because -> content += content 
+          // We want to add on previous content at the same time
+          content = `${content}<tr id='${response[i]._id}'>
+          <td>${response[i].userName}</td>
+          <td>${response[i].userEmail}</td>
+          <td>${response[i].userPwd}</td>
+          <td><a href='#' class='delete' data-id='${response[i]._id}'>Del</a></td>
+          <td><a href='#update-contact-container' class='update' data-id='${response[i]._id}' data-name='${response[i].userName}' data-sID='${response[i].userEmail}' data-sMentor='${response[i].userPwd}'>Update</a></td></tr>`;
+
+        }
+
+        //[STEP 9]: Update our HTML content
+        // Let's dump the content into our table body
+        document.getElementById("contact-list").getElementsByTagName("tbody")[0].innerHTML = content;
+
+        document.getElementById("total-contacts").innerHTML = response.length;
+      });
+  }
+
+
+
+
+});
